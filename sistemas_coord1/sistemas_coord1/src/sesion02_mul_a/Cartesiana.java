2a3bcc2ce5a7fac6deba7b3c4b149e3947a814e7
JuanesWagner <79537999+JuanesWagner@users.noreply.github.com>
2021-08-09 09:22:00 -0500

    Add files via upload

---
+++ sistemas_coord1/sistemas_coord1/src/sesion02_mul_a/Cartesiana.java
@@ -0,0 +1,63 @@
+package sesion02_mul_a;
+
+
+public class Cartesiana {
+    private float x;
+    private float y;
+    
+
+    public Cartesiana(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Cartesiana(){
+    
+       
+    }
+
+public Cartesiana polar_cartesiano (float radio, float angulo) {
+//float x = radio * (float)Math.cos(angulo); // implementar la formula correcta
+//float y = radio * (float)Math.sin(angulo); // implementar la forumula correcta 
+//return new Cartesiana(x,y);
+  return new Cartesiana (radio * (float)Math.cos(angulo),radio * (float)Math.sin(angulo));  
+}
+
+public Cartesiana polar_cartesiano (Polar p){
+ 
+   // float x = p.getRadio()*(float)Math.cos(p.getAngulo());
+    //float y = p.getRadio()*(float)Math.sin(p.getAngulo());
+           
+     return polar_cartesiano (p.getRadio(),p.getAngulo()); // implementar procedimiento correcto
+}
+
+
+public Polar cartesiano_polar(float x, float y){
+
+float radio = (float)Math . sqrt    (Math . pow(x,2)+ Math . pow(y,2));
+float angulo = (float)Math . atan(y/x); 
+return new Polar(radio,angulo);
+}
+  
+   public Polar cartesiano_polar (Cartesiana c){
+   
+   return cartesiano_polar (c.getX(),c.getY()); // implementar procedimiento correcto
+   }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+    
+}
